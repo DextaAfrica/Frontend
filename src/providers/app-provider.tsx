dbad9c1ba6073ea133abeb25faedb6bb82ac46fb
peterchims <peterbozz1996@gmail.com>
2026-08-03 12:43:46 +0100

    fixes-frontend-UI

--- src/providers/app-provider.tsx
+++ src/providers/app-provider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import * as React from "react";
+import { MotionConfig } from "motion/react";
 import { ConsentManager } from "@/components/consent/consent-manager";
 import { NewsletterManager } from "@/features/newsletter/components/newsletter-manager";
 import { ThemeProvider } from "@/providers/theme-provider";
@@ -8,9 +9,11 @@ import { ThemeProvider } from "@/providers/theme-provider";
 export function AppProvider({ children }: React.PropsWithChildren) {
   return (
     <ThemeProvider>
-      {children}
-      <ConsentManager />
-      <NewsletterManager />
+      <MotionConfig reducedMotion="user">
+        {children}
+        <ConsentManager />
+        <NewsletterManager />
+      </MotionConfig>
     </ThemeProvider>
   );
 }
